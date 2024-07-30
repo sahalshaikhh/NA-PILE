844b15b717349f3d01ee7d8001cfcd228b71587b
Sahal-shaikh <sahal.shaikh@reveation.io>
2024-07-30 18:56:09 +0530

    fix:loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -130,7 +130,7 @@ function Home() {
               variants={headingVariants}
             >
               <h1 className="what-we-offer text-center font-semibold text-3xl mb-8 sm:text-4xl lg:text-5xl  lg:mt-28 lg:mb-28 lg:bg-[#181717] lg:py-8  xl:text-4xl  xl:my-24 ">
-                Client Testimonials
+                Client Testimonialsss
               </h1>
             </motion.div>
           )}
